9ba5933e15b6b5b66fde81a537865986aff62336
ximenxiaoxue <1732373074@qq.com>
2024-09-21 15:30:53 +0800

    实现消息的共享

--- API_LIST/API_main_dispatcher.py
+++ API_LIST/API_main_dispatcher.py
@@ -1,29 +1,29 @@
 
 import asyncio
 
-from API_LIST.basic_QQapi import Msg_process_api
+from API_LIST.basic_QQapi import Msg_process_api,Msg_answer_api
 from basic_api.Share_data import Raw_data,Processed_data
 
 class Msg_dispatcher:
-    def __init__(self):
-        pass
-    async def dispatch(self):
+    def __init__(self,websocket):
+        self.websocket = websocket
+
+    async def dispatch(self): # 接收消息并分发给相应的处理函数,此处只进行消息的简单处理
         while True:
             msg = await Raw_data.get()
-            #print(type(msg))
             await Msg_process_api.Msg_process(msg).Message_processor()
 
-    async def msg_dispatch_task(self):
+    async def msg_dispatch_task(self): # 接收处理后的消息并进行处理，此处注册需要用到的处理函数
         while True:
             msg = await Processed_data.get()
+            answer_msg = asyncio.create_task(Msg_answer_api.Answer_api(self.websocket,msg).msg_answer_all())
+
 
 
     async def dispatch_task_main(self):
         msg_dispatch_task_1 = asyncio.create_task(self.dispatch())
-        msg_dispatch_task_2 = asyncio.create_task(self.dispatch())
+        msg_dispatch_task_2 = asyncio.create_task(self.msg_dispatch_task())
 
-if __name__ == '__main__':
-    msg_dispatcher = Msg_dispatcher()
 
 
 
