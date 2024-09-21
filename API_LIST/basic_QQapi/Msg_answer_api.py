58f2d11f9087ac9c8cc0cba3923a9e43eddefb07
ximenxiaoxue <1732373074@qq.com>
2024-09-21 15:31:43 +0800

    优化回复函数

--- API_LIST/basic_QQapi/Msg_answer_api.py
+++ API_LIST/basic_QQapi/Msg_answer_api.py
@@ -2,100 +2,121 @@ import yaml
 from random import randint
 import aiohttp
 import json
+import pandas as pd
 
 from basic_api.Logger_owner import Logger
 from basic_api.Botapi_List import QQAPI_list
+
+
 
 with open('_config.yml', 'r', encoding='utf-8') as f:
     config = yaml.load(f.read(), Loader=yaml.FullLoader)
 
 
-class answer_api:
-    def __init__(self, websocket, Raw_data_queue):
+class Answer_api:
+    def __init__(self,websocket,Processed_data ):
 
         self.Logger = Logger()
 
-        self.Raw_data = Raw_data_queue
+        self.Processed_data = Processed_data
         self.websocket = websocket
-        self.random_num = config["bot_api_settings"]["random_answer"]["random_num"]
-        self.test_config = config["test_settings"]["test_enable"]
-        self.judgement_api = config["bot_api_settings"]["answer_enable"]
-        self.judgement_random = config['bot_api_settings']['random_answer']['enable']
 
-    async def message_answer_api(self):
 
-        data_processing = await self.Raw_data.get()
+        self.random_num = config["bot_api_settings"]["random_answer"]["random_num"]  # 随机回复概率
+        self.test_config = config["test_settings"]["test_enable"]  # 测试模式
+        self.judgement_api = config["bot_api_settings"]["answer_enable"]  # 开启api接口
+        self.judgement_random = config['bot_api_settings']['random_answer']['enable']  # 开启随机回复
+        self.judgement_dict_answer = config["bot_api_settings"]["dict_answer"]["enable"]  # 开启词典回复
 
-        answer_api_dict = await self.message_answer_logic_dict(data_processing)
-        if answer_api_dict == False:
-            answer_api_basic = await self.message_answer_logic_basic(data_processing)
-        elif answer_api_dict == None:
-            pass
-        else:
-            pass
+    async def msg_random_answer(self):
 
-    async def message_answer_logic_basic(self, data_processing):
-        if self.judgement_api == False:  # 判断是否开启api接口
-            return False
-        if data_processing["judgement_at"] == True or data_processing["message_group"] == None:  # 判断是否@机器人
-            await self.message_getanswer_random(data_processing)
+        if self.Processed_data["judgement_at"] == True or self.Processed_data["message_group"] == None:  # 判断是否@机器人
+            await self.msg_answer_api()
 
-        elif data_processing["judgement_at_other"] == True:  # 判断是否@其他人
+        elif self.Processed_data["judgement_at_other"] == True:  # 判断是否@其他人
             pass
         else:
             if self.judgement_random == True:  # 判断是否开启随机回复
                 random_num = randint(1, 100)
-                # print("随机数：", random)
+                print("随机数：", random_num)
                 if random_num <= self.random_num:  # 随机回复
-                    await self.message_getanswer_random(data_processing)
+                    await self.msg_answer_api()
                 else:
                     pass
             else:
-                await self.message_getanswer_random(data_processing)
+                await self.msg_answer_api()
         if self.test_config == True:  # 判断是否开启测试模式
-            self.Logger.warning(f"测试模式(data_processing)：{data_processing}" +
-                                "\n" +
-                                f"随机数：{random_num}" if "random_num" in locals() else "")
+            self.Logger.warning(f"测试模式(self.Processed_data)：{self.Processed_data}" +
+                            "\n" +
+                            f"随机数：{random_num}" if "random_num" in locals() else "")
 
-    async def message_answer_logic_dict(self, data_processing):
-        judgement_dict = await self.message_dict_api(data_processing)
-        if judgement_dict == False:
-            return False
-
-    async def message_getanswer_random(self, data_processing):  # 获取api接口的回复并发送消息
-
-        # print(data_processing)
-        message = data_processing['message_content']
-        url = f"http://api.qingyunke.com/api.php?key=free&appid=0&msg={message}"
-
+    async def msg_answer_api(self):
+        msg = self.Processed_data["message_content"]
+        url = f"http://api.qingyunke.com/api.php?key=free&appid=0&msg={msg}"
         async with aiohttp.ClientSession() as session:
             async with session.get(url) as response:
                 result = await response.text()
-                answer = json.loads(result)['content']
+                result_dict = json.loads(result)
+                answer = result_dict["content"]
+                await self.msg_send_api(answer)
 
-            await self.message_sendanswer_api(data_processing, answer)
-
-    async def message_dict_api(self, data_processing):  # 指令注册，及API函数注册
-        content = data_processing['message_content']
-        if content == "help":
-            message = ("指令列表：\n"
-                       "暂无\n")
-            await self.message_sendanswer_api(data_processing, message)
-            pass
-
-        else:
+    async def msg_dict_answer(self):  # 词典回复
+        if self.judgement_dict_answer == False:  # 判断是否开启api接口
             return False
+        content = self.Processed_data["message_content"]
+        answer = await self.dict_find_answer(content)
+        if answer == None:
+            return False
+        else:
+            await self.msg_send_api(answer)
+            return True
 
-    async def message_sendanswer_api(self, data, answer):  # 发送消息
-        if data["message_group"] == None:
+    async def dict_find_answer(self,target_text): # 词典寻找答案
+        # 读取Excel文件
+        df = pd.read_csv('./resource/dict_answer.csv', encoding='GBK')
+        # 假设我们要在名为'column_name'的列中查找包含特定文字的单元格
+        # 并且我们想要获取该列后面一列的数据
+        column_name = 'question'  # 替换为你的列名
+        next_column_name = df.columns[df.columns.get_loc(column_name) + 1]  # 获取后面一列的列名
+        # 找到包含特定文字的行
+        filtered_df = df[df[column_name].str.contains(target_text, na=False)]
+        # 获取后面一列的数据
+        answers = filtered_df[next_column_name].tolist()
+
+        if len(answers) == 0:
+            #print("没有找到答案")
+            return None
+        else:
+            #print("词典回复：" + answers[0])
+            return answers[0]
+
+    async def msg_send_api(self,answer):
+        if self.Processed_data["message_group"] == None:
             # 私聊消息
-            user_id = data['message_sender_id']
+            user_id = self.Processed_data['message_sender_id']
             await QQAPI_list(self.websocket).send_message(user_id, answer)
             pass
         else:
             # 群聊消息
-            group_id = data['message_group']
+            group_id = self.Processed_data['message_group']
             await QQAPI_list(self.websocket).send_group_message(group_id, answer)
+
+
+    async def msg_answer_all(self):
+        if self.judgement_api == False:  # 判断是否开启api接口
+            return
+        if self.judgement_dict_answer == False:  # 判断是否开启api接口
+            judgement_dict = None
+        else:
+            judgement_dict = await self.msg_dict_answer()
+            #print(judgement_dict)
+
+        if judgement_dict == None or judgement_dict == False:
+            await self.msg_random_answer()
+            #print("随机回复")
+        else:
             pass
+
+
 
 
